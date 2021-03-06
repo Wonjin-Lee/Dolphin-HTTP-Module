9ffad1cdf2339af0e18d01f600236376ff043744
Wonjin-Lee <claude17@naver.com>
2021-03-07 00:33:24 +0900

    [기능] HTTP 관련 상수 공개용 클래스 생성
    + log4j 의존성 추가

---
+++ src/main/java/com/wonjin/dolphin/constants/HTTPConstants.java
@@ -0,0 +1,9 @@
+package com.wonjin.dolphin.constants;
+
+public class HTTPConstants {
+    private HTTPConstants() { }
+
+    public static final String TLSv1 = "TLSv1";
+    public static final String TLSv1_1 = "TLSv1.1";
+    public static final String TLSv1_2 = "TLSv1.2";
+}
