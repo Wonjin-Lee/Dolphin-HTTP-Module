5e7b43e59718e45986cca9bb0ceca5833bc4b7b4
Wonjin-Lee <claude17@naver.com>
2021-03-07 00:59:34 +0900

    [기능] HttpClient 인스턴스 생성을 위한 기능 개발
    1. HttpClient 인스턴스 생성에 사용될 설정값 관련 멤버변수 선언
    2. 멤버변수 초기화를 위한 setter 생성
    3. 프로토콜(HTTP/HTTPS) 선택에 따라 분기 처리하여 HttpClient 생성하도록 메서드 정의

--- src/main/java/com/wonjin/dolphin/http/HTTPManager.java
+++ src/main/java/com/wonjin/dolphin/http/HTTPManager.java
@@ -1,4 +1,94 @@
 package com.wonjin.dolphin.http;
 
+import com.wonjin.dolphin.http.protocol.Protocol;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.log4j.Logger;
+
+import java.util.Map;
+
 public class HTTPManager {
+    private static Logger logger = Logger.getLogger(HTTPManager.class);
+
+    private int maxConnectionsPerRoute;
+    private int maxConnectionsTotal;
+
+    private int connectionTimeout;
+    private int connectionRequestTimeout;
+    private int socketTimeout;
+
+    private Protocol protocol;
+
+    private String url;
+
+    private Map<String, String> headerMap;
+    private Map<String, String> parameterMap;
+
+    /**
+     * Create HTTP/HTTPS Client
+     */
+    private CloseableHttpClient getClient() {
+        if (Protocol.HTTP == protocol) {
+            return createHttpClient();
+        }
+        return createSSLHttpClient();
+    }
+
+    /**
+     * HTTP Client
+     */
+    private CloseableHttpClient createHttpClient() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+
+        connectionManager.setMaxTotal(maxConnectionsTotal);
+        connectionManager.setDefaultMaxPerRoute(maxConnectionsPerRoute);
+
+        return HttpClients.custom().setConnectionManager(connectionManager)
+                .setRedirectStrategy(new LaxRedirectStrategy()).build();
+    }
+
+    /**
+     * SSL HTTP Client
+     */
+    private CloseableHttpClient createSSLHttpClient() {
+        return null;
+    }
+
+    public void setMaxConnectionsPerRoute(int maxConnectionsPerRoute) {
+        this.maxConnectionsPerRoute = maxConnectionsPerRoute;
+    }
+
+    public void setMaxConnectionsTotal(int maxConnectionsTotal) {
+        this.maxConnectionsTotal = maxConnectionsTotal;
+    }
+
+    public void setConnectionTimeout(int connectionTimeout) {
+        this.connectionTimeout = connectionTimeout;
+    }
+
+    public void setConnectionRequestTimeout(int connectionRequestTimeout) {
+        this.connectionRequestTimeout = connectionRequestTimeout;
+    }
+
+    public void setSocketTimeout(int socketTimeout) {
+        this.socketTimeout = socketTimeout;
+    }
+
+    public void setProtocol(Protocol protocol) {
+        this.protocol = protocol;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setHeaderMap(Map<String, String> headerMap) {
+        this.headerMap = headerMap;
+    }
+
+    public void setParameterMap(Map<String, String> parameterMap) {
+        this.parameterMap = parameterMap;
+    }
 }
